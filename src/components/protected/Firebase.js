96ef4723e2086b629f4ff31c3d6c7304a7cfb07b
koren hamra <56797632+korenh@users.noreply.github.com>
2020-12-30 11:02:33 +0200

    Update Firebase.js

--- src/components/protected/Firebase.js
+++ src/components/protected/Firebase.js
@@ -4,14 +4,7 @@ import "firebase/auth";
 import "firebase/firestore";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyBQ1sLH6IFYHC2gXzCHEpaiTtLmKdqCrvM",
-  authDomain: "tasking-993d0.firebaseapp.com",
-  databaseURL: "https://tasking-993d0.firebaseio.com",
-  projectId: "tasking-993d0",
-  storageBucket: "tasking-993d0.appspot.com",
-  messagingSenderId: "590535416607",
-  appId: "1:590535416607:web:93ebfcdf9a2759ec5f2e2a",
-  measurementId: "G-Z3T7FTG1P9",
+  "use your own config"
 };
 
 firebase.initializeApp(firebaseConfig);
